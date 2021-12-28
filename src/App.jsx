13735d418d268baab549ff1aea5653ecf80db613
Yuriy Sobolyev <ura1111@gmail.com>
2021-12-28 14:50:24 +0200

    first tuning weather app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/f6/OpenWeather-Logo.jpg" className="App-logo" alt="logo" />
+        <p>
+          Weather App.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
